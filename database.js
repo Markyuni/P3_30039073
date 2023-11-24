4b41de305f41cc69092c4393c036b253c661d353
Markyuni <132387560+Markyuni@users.noreply.github.com>
2023-11-24 17:40:31 -0400

    Add files via upload

--- database.js
+++ database.js
@@ -1,33 +1,174 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database(':memory:', (err) => {
+let db = new sqlite3.Database('./lentesSol.db', (err) => {
     if (err) {
         return console.error(err.message);
     }
-    console.log('Connected to the in-memory SQlite database.');
+    console.log('Connected to the SQlite database.');
 
-    db.run("CREATE TABLE IF NOT EXISTS categorias (nombre TEXT NOT NULL)");
-    db.run("CREATE TABLE IF NOT EXISTS productos (nombre TEXT NOT NULL, color TEXT NOT NULL, talla TEXT NOT NULL, codigo TEXT NOT NULL, precio INTEGER NOT NULL, descripcion TEXT NOT NULL, categoria_id INTEGER PRIMARY KEY AUTOINCREMENT)")
-    db.run("CREATE TABLE IF NOT EXISTS imagenes (producto_id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT NOT NULL, destacado TEXT NOT NULL)")
+    db.run("CREATE TABLE IF NOT EXISTS categorias (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT NOT NULL)");
+    db.run("CREATE TABLE IF NOT EXISTS productos (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT NOT NULL, color TEXT NOT NULL, talla TEXT NOT NULL, codigo TEXT NOT NULL, precio FLOAT NOT NULL, descripcion TEXT NOT NULL, categoria_id INTEGER NOT NULL, FOREIGN KEY (categoria_id) REFERENCES categorias (id) ON DELETE CASCADE)");
+    db.run("CREATE TABLE IF NOT EXISTS imagenes (id INTEGER PRIMARY KEY AUTOINCREMENT, url STRING, destacado TEXT, producto_id INTEGER, FOREIGN KEY (producto_id) REFERENCES productos (id) ON DELETE CASCADE)");
+
+    db.get("PRAGMA foreign_keys = ON");
 });
 
 
 module.exports = {
-    insert: function (name, email, comment, date, myIP, pais, contraseña) {
-        db.run("INSERT INTO contactos (name, email, comment, date, myIP, pais, contraseña) VALUES (?, ?, ?, ?, ?, ?)", [name, email, comment, date, myIP, pais, contraseña], function (err) {
+    insertProducto: function (nombre, color, talla, codigo, precio, descripcion, categoria_id) {
+        db.run("INSERT INTO productos (nombre, color, talla, codigo, precio, descripcion, categoria_id) VALUES (?, ?, ?, ?, ?, ?, ?)", [nombre, color, talla, codigo, precio, descripcion, categoria_id], function (err) {
             if (err) {
                 return console.log(err.message);
             }
             // get the last insert id
-            console.log(`A row has been inserted with rowid ${this.lastID}`);
+            console.log(`A row in "productos" has been inserted with rowid ${this.lastID}`);
         });
     },
-    select: function (callback) {
-        db.all("SELECT * FROM contactos", [], (err, rows) => {
+    insertImagen: function (url, producto_id, destacado) {
+        db.run("INSERT INTO imagenes (url, producto_id, destacado) VALUES (?, ?, ?)", [url, producto_id, destacado], function (err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            //get the last insert id
+            console.log(`A row in "imagenes" has been inserted with rowid ${this.lastID}`);
+        });
+    },
+    insertCategoria: function (nombre) {
+        db.run("INSERT INTO categorias (nombre) VALUES (?)", [nombre], function (err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            //get the last insert id
+            console.log(`A row in "categorias" has been inserted with rowid ${this.lastID}`);
+        });
+    },
+    updateProducto: function (nombre, color, talla, codigo, precio, descripcion, categoria_id, id) {
+        db.run("UPDATE productos SET (nombre, color, talla, codigo, precio, descripcion, categoria_id) = (?, ?, ?, ?, ?, ?, ?) WHERE id = ?", [nombre, color, talla, codigo, precio, descripcion, categoria_id, id], function (err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            //get the last updated id
+            console.log(`A row in "productos" has been updated on rowid: `, id);
+        });
+    },
+    updateCategoria: function (nombre, id) {
+        db.run("UPDATE categorias SET nombre = ? WHERE id = ?", [nombre, id], function (err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            //get the last updated id
+            console.log(`A row in "categorias" has been updated on rowid: `, id);
+        });
+    },
+    updateImagen1: function (callback) {
+        db.all("SELECT * FROM imagenes", [], (err, rows) => {
             if (err) {
                 throw err;
             }
             callback(rows);
+        });
+    },
+    updateImagen2: function (url, producto_id, destacado, id) {
+        db.run("UPDATE imagenes SET (url, producto_id, destacado) = (?, ?, ?) WHERE id = ?", [url, producto_id, destacado, id], function (err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            //get the last updated id
+            console.log(`A row in "imagenes" has been updated on rowid: `, id);
+        });
+    },
+    deleteCategoria: function (id) {
+        db.run("DELETE FROM categorias WHERE id = ?", [id], function (err) {
+            if (err) {
+                throw err;
+            } else {
+                db.run("UPDATE `main`.`sqlite_sequence` SET `seq` = '1' WHERE  `name` = 'categorias'", function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                    //get the last deleted id
+                    console.log(`A row in "categorias" has been deleted on rowid: `, id);
+                });
+            };
+        });
+    },
+    deleteProducto: function (id) {
+        db.run("DELETE FROM productos WHERE id = ?", [id], function (err) {
+            if (err) {
+                throw err;
+            } else {
+                db.run("UPDATE `main`.`sqlite_sequence` SET `seq` = '1' WHERE  `name` = 'productos'", function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                    //get the last deleted id
+                    console.log(`A row in "productos" has been deleted on rowid: `, id);
+                });
+            };
+        });
+    },
+    deleteImagen: function (id) {
+        db.run("DELETE FROM imagenes WHERE id = ?", [id], function (err) {
+            if (err) {
+                throw err;
+            } else {
+                db.run("UPDATE `main`.`sqlite_sequence` SET `seq` = '1' WHERE  `name` = 'imagenes'", function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                    //get the last deleted id
+                    console.log(`A row in "imagenes" has been deleted on rowid: `, id);
+                });
+            };
+        });
+    },
+    selectProductoImagen: function (callback) {
+        db.all("SELECT * FROM productos", [], (err, rows) => {
+            if (err) {
+                throw err;
+            } else {
+                db.all("SELECT * from imagenes ORDER BY producto_id, destacado DESC", [], (err, rows2) => {
+                    if (err) {
+                        throw err;
+                    }
+                    callback(rows, rows2);
+                });
+            };
+        });
+    },
+    selectCategoria: function (callback) {
+        db.all("SELECT * FROM categorias", [], (err, rows) => {
+            if (err) {
+                throw err;
+            }
+            callback(rows);
+        });
+    },
+    selectProdImg: function (callback) {
+        db.all("SELECT * FROM productos", [], (err, info_prod) => {
+            if (err) {
+                throw err;
+            } else {
+                db.all("SELECT url, producto_id FROM imagenes WHERE destacado = 1", [], (err, info_img) => {
+                    if (err) {
+                        throw err;
+                    }
+
+                    // Crear un nuevo arreglo que combina info_prod e info_img
+                    const combined = info_prod.map(prod => {
+                        // Encontrar la imagen correspondiente para este producto
+                        const img = info_img.find(i => i.producto_id === prod.id);
+
+                        // Devolver un nuevo objeto que combina la información del producto e imagen
+                        return {
+                            ...prod,
+                            img: img ? img.url : null
+                        };
+                    });
+
+                    callback(combined);
+                });
+            };
         });
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,185 @@
 var express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const db = require('../database');
 var router = express.Router();
 
-/* GET home page. */
+/* GET login page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Datos del Estudiante',
-    nombre: 'Jesús', apellido: 'Ramírez',
-    cedula: '30.039.073',
-    seccion: '1'
+  res.render('login');
+});
+
+/* GET admin home page. */
+router.get('/index_admin', function(req, res, next) {
+  res.render('index_admin');
+});
+
+/* GET public home page */
+router.get('/index_public', function(req, res, next) {
+  db.selectProdImg((data) => {
+    console.log(data)
+    res.render('index_public', { products: data });
   });
 });
 
-router.post('/', function(req, res, next) {
+/* GET Product Listing page. */
+router.get('/producto_listado', function(req, res, next) {
+  db.selectProductoImagen((rows, rows2) => {
+    console.log(rows, rows2);
+    res.render("producto_listado", {
+      data: rows,
+      data2: rows2
+    });
+  });
+});
 
-  res.redirect('/');
+/* GET Insert Product page. */
+router.get('/producto_insertar', function(req, res, next) {
+  res.render('producto_insertar');
+});
+
+/* GET Modify Product page. */
+router.get('/producto_modificar', function(req, res, next) {
+  res.render('producto_modificar');
+});
+
+/* GET Insert Image page. */
+router.get('/imagen_agregar', function(req, res, next) {
+  res.render('imagen_agregar');
+});
+
+/* GET Modify Image page. */
+router.get('/imagen_modificar', function(req, res, next) {
+  db.updateImagen1((rows) => {
+    console.log(rows);
+    res.render("imagen_modificar", { data: rows });
+  });
+});
+
+/* GET Category Listing page. */
+router.get('/categoria_listado', function(req, res, next) {
+  db.selectCategoria((rows) => {
+    console.log(rows);
+    res.render("categoria_listado", { data: rows });
+  });
+});
+
+/* GET Insert Category page. */
+router.get('/categoria_insertar', function(req, res, next) {
+  res.render('categoria_insertar');
+});
+
+/* GET Modify Category page. */
+router.get('/categoria_modificar', function(req, res, next) {
+  res.render('categoria_modificar');
+});
+
+router.post('/login', function(req, res, next) {
+  let admin = req.body.admin;
+  let pwd = req.body.pwd;
+
+  console.log({ admin, pwd });
+
+  if (admin === process.env.ADMIN && pwd === process.env.PASSWORD) {
+    res.redirect('/index_admin');
+  } else {
+    res.redirect('/');
+  }
+});
+
+router.post('/insertarProducto', function(req, res, next) {
+  let nombre = req.body.nombre;
+  let color = req.body.color;
+  let talla = req.body.talla;
+  let codigo = req.body.codigo;
+  let precio = req.body.precio;
+  let descripcion = req.body.descripcion;
+  let categoria_id = req.body.categoria;
+
+  console.log({ nombre, color, talla, codigo, precio, descripcion, categoria_id });
+
+  db.insertProducto(nombre, color, talla, codigo, precio, descripcion, categoria_id);
+
+  res.redirect('/producto_insertar');
+});
+
+router.post('/eliminarProducto', function(req, res, next) {
+  let id = req.body.id;
+
+  console.log({ id });
+
+  db.deleteProducto(id);
+
+  res.redirect('/producto_listado');
+});
+
+router.post('/modificarProducto', function(req, res, next) {
+  let nombre = req.body.nombre;
+  let color = req.body.color;
+  let talla = req.body.talla;
+  let codigo = req.body.codigo;
+  let precio = req.body.precio;
+  let descripcion = req.body.descripcion;
+  let categoria_id = req.body.categoria;
+  let id = req.body.id;
+
+  console.log({ nombre, color, talla, codigo, precio, descripcion, categoria_id, id });
+
+  db.updateProducto(nombre, color, talla, codigo, precio, descripcion, categoria_id, id);
+
+  res.redirect('/producto_modificar');
+});
+
+router.post('/agregarImagen', function(req, res, next) {
+  let url = req.body.url;
+  let producto_id = req.body.producto_id;
+  let destacado = req.body.destacado;
+
+  console.log({ url, producto_id, destacado });
+
+  db.insertImagen(url, producto_id, destacado);
+
+  res.redirect('/imagen_agregar');
+});
+
+router.post('/eliminarImagen', function(req, res, next) {
+  let id = req.body.id;
+
+  console.log({ id });
+
+  db.updateImagen(id);
+
+  res.redirect('/producto_listado');
+});
+
+router.post('/modificarImagen1', function(req, res, next) {
+  let id = req.body.id;
+
+  console.log({ id });
+
+  db.updateImagen1(id);
+
+  res.redirect('/imagen_modificar')
+})
+
+router.post('/insertarCategoria', function(req, res, next) {
+  let nombre = req.body.nombre;
+
+  console.log({ nombre });
+
+  db.insertCategoria(nombre);
+
+  res.redirect('/categoria_insertar');
+});
+
+router.post('/modificarCategoria', function(req, res, next) {
+  let nombre = req.body.nombre;
+  let id = req.body.id;
+
+  console.log({ nombre, id });
+
+  db.updateCategoria(nombre, id);
+
+  res.redirect('/categoria_modificar');
 });
 
 module.exports = router;
